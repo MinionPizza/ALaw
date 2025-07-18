7826fa92cd52c14a795e80e76321a8f1aa9b0c2b
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-18 12:56:37 +0900

    fix : 프로젝트 명 수정 LawvatarBackend ->Backend
    Former-commit-id: 75436c80f88cf1903d46bc161845ccb9f76a5fb3

--- backend/src/main/java/com/B204/lawvatar_backend/LawvatarBackendApplication.java
+++ backend/src/main/java/com/B204/lawvatar_backend/LawvatarBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LawvatarBackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LawvatarBackendApplication.class, args);
 	}
