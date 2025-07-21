27d81f5fe594af47c53818886a54b33da3d1acd2
전해지 <wjsgowl0224@naver.com>
2025-07-21 23:40:14 +0900

    feat: 엔티티들 생성 및 연관관계 매핑
    Former-commit-id: ebb31a1a69ab105d134320099d7b7291301133fb

---
+++ backend/src/main/java/com/B204/lawvatar_backend/common/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.B204.lawvatar_backend.common.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+}
---
+++ backend/src/main/java/com/B204/lawvatar_backend/common/entity/LawyerTag.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8058cedc529ee48e65a5f8feb8b90183546f0db134e73c4a0f26722f57bc9d4
+size 521
