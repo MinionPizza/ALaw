e175b62528f032a675700bcd895bb58c9e3afbff
Grayson1999 <bbt1250912@gmail.com>
2025-07-23 21:10:49 +0900

    feat: 필요 라이브러리 설치 및 PostgreSQL 클라이언트 초기화 코드 추가
    - conda 환경 파일(environment.yml)에 chromadb, numpy 패키지 추가
    - database.py: SQLAlchemy 기반 PostgreSQL 연결 초기화 및 세션 관리 코드 구현
    - config/db_config.py: DB 설정 정보 외부 모듈로 분리
    
    S13P11B204-194
    
    
    Former-commit-id: 25e0a239ad5472ec0d1dbb466a9cc066b954ab46

---
+++ ai/config/db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+# 데이터베이스 설정을 위한 구성 파일
+
+DB_USER = "postgres"
+DB_PASSWORD = "1250"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "postgres"
---
+++ ai/db/database.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ebce800e2ffc1f7439f292f2c61a72d01f33050fe15bb249919e0d2b85d6f699
+size 1562
