be02af4b93e4c324ac62121fcbb0d2ea1aa30937
grayson(방승철) <bbt1250912@gmail.com>
2025-07-24 01:38:09 +0900

    feat: 법률 검색 인프라 초기 구축 완료
    - 원본 판례 JSON을 RDBMS에 덤프 및 적재
    - 벡터 테이블 생성, 임베딩 저장 로직 및 HNSW 인덱스(m=12, ef_construction=150) 스크립트 구현
    - HNSW 인덱스 파라미터 적용 여부 확인 및 검증
    
    S13P11B204-194
    
    
    Former-commit-id: 4f4cb3770cb7e606cb3699ef3e05522ef59acef6

---
+++ ai/scripts/build_index.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8e26163a8cf44adfc729971dcecd0a4a61eea0290c5daad09ea989e58b0b44e
+size 4460
---
+++ ai/utils/logger.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f8320c52461261d50769ffc490ca6169f07d05acf5af5c86af3b41d505197ed
+size 616
