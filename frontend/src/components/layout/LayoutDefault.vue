10f16e357181d9d233f1053d47ff71877cbc04f8
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-07-23 13:35:05 +0900

    router전달
    Former-commit-id: 064ebb5ed9962745c198936d164d860dc4c9ac1f

---
+++ frontend/src/components/layout/LayoutDefault.vue
@@ -0,0 +1,20 @@
+<template>
+  <Navbar />
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
+
+<style scoped>
+.container {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 0 40px;
+  box-sizing: border-box;
+  min-height: 100vh;
+}
+</style>
---
+++ frontend/src/features/auth/user/OAuthCallback.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6db18de345cc4449089666e7d65ad6bd3e277e82f145357a4798aa0dbef6ce90
+size 776
