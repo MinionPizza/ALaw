279aa0aaccfd1794cc176617157cddead4cd46de
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-07-23 15:12:19 +0900

    feat: 네비게이션바 추가
    Former-commit-id: 3f67b8013bc8c26dc330b9bbb56c9dd678bf7cbf

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bd8fc0955a37dd788215da94dc0c2dc5fcfdf14a917605792cf9d0792e34a1e
-size 1128
+oid sha256:e8a6c1a7e0fc5b7a13866e37740c0a040923a10cd07f1e0e51e83e335967eb72
+size 1082
