194b5a8cd9ace141654ef1762be23874901ead02
jeongyeon <wjddusdl921@naver.com>
2025-07-23 13:55:24 +0900

    fix : 라우터수정 및 회원가입 폼 초안 생성
    Former-commit-id: 5c186f939d1a23f93aef988bcece0c97eee70b8e

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:431e985e9420df36107f6a7c60fbf0353add5ffbbecd7a33971a787c8d3fc857
-size 1167
+oid sha256:749c57588610753927d4a6c085846e2f02bf46db4267eb24bae4dfcb85824db0
+size 1212
