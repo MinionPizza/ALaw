7e19a0256eb68baaac143920961d3ae548f7183a
김태인 <inintae63@gmail.com>
2025-07-24 16:16:21 +0900

    fix. merge충돌 후 오류 해결
    Former-commit-id: 64f1baa44e3f8f3f2efe57e75feaac1793002181

--- frontend/src/mocks/browser.js
+++ frontend/src/mocks/browser.js
@@ -1,5 +1,5 @@
 // src/mocks/browser.js
-import { setupWorker } from 'msw'
+import { setupWorker } from 'msw/browser' // 이 경로는 올바르게 수정되었습니다.
 import { handlers } from './handlers'
 
 // 브라우저에서 사용할 수 있는 worker 생성
