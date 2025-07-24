a0b9233c45ad2c0b8b3b99f2211dca22b94b6df0
김태인 <inintae63@gmail.com>
2025-07-24 11:17:34 +0900

    feat. 판례 검색 및 조회 구현
    판례 검색 후 리스트 구현 및 클릭시 상세조회 라우터로 이동
    
    
    Former-commit-id: cac71047076a79801292eaeba7cad51e0025c2e6

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b4df41be214a8634559715bd59efad7e06d7f8dfec8ae6b7a9d937e7676518f
-size 1991
+oid sha256:e052f5c19fce558e8300d560126c7ed6bc4488361e64085dae83bf7cffbd07ab
+size 2179
