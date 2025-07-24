c6524311bbf0803e6300bdb50d03431d89e1635c
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-07-24 15:10:28 +0900

    feat: 화상상담 서로 얼굴공유
    Former-commit-id: 25753ce06d9c1d555c19b69d36835e27c2a27c2a

--- frontend/src/features/auth/user/OAuthCallback.vue
+++ frontend/src/features/auth/user/OAuthCallback.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6db18de345cc4449089666e7d65ad6bd3e277e82f145357a4798aa0dbef6ce90
-size 776
+oid sha256:ded66988b6585cf9657a93aca28543ca8f17a4feea698496cf96205d60b8f2a0
+size 750
--- frontend/src/features/videoconference/user/PreviewUserView.vue
+++ frontend/src/features/videoconference/user/PreviewUserView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa5fd3291773f13eaeb09466bef9c59802d53ca49c80a2b483e4a8e37c169ac9
-size 1899
+oid sha256:2d2f4b57451e895dc8f3560cff60e83d5be757a0bf75e7d9ef48fcb5369f4536
+size 2406
