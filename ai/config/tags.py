ec968f7d9d305bbc85d8dd4a40d094a6dac9b50f
grayson(방승철) <bbt1250912@gmail.com>
2025-07-25 16:17:29 +0900

    feat(: LLM 기반 법률 사건 태그 구현
    - config/tags.py에 전문 분야 태그 목록 정의
    - llm/prompt_templates/tag_classifier_prompt.py에 분류용 프롬프트 템플릿 작성
    
    S13P11B204-207
    
    
    Former-commit-id: 2d26d594fdd124b10a0baf24d2e7ffe955f27377

---
+++ ai/config/tags.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:91f28f170c48e7bbc3bae0a0c9651c0980cbb7368d5b096c3b700323ba71f8e7
+size 997
--- ai/services/case_analysis_service.py
+++ ai/services/case_analysis_service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7feb6155fc3b9071e3e4fee5bdcc4665067e2c56dd4d69ac8ffa7db1109f017f
-size 3620
+oid sha256:655de1c2a105b42b3c26ebcb61d9e608302fa23ae4884b5f63f68061bdf02510
+size 3827
