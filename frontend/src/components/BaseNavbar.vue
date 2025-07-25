25170a3555bb8bf36025e242a75133dd7465d4c8
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-07-25 15:12:07 +0900

    feat: 판례조회 디자인
    Former-commit-id: fd4413ff88196d86439df8ef8f2e566a9f7d474f

--- frontend/src/components/BaseNavbar.vue
+++ frontend/src/components/BaseNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d7a0b4e56d25954ce49e65ea5aacecb26f8e1551a2567440c070db41937ffc7
-size 1086
+oid sha256:fc79ec2ccf6a0e06c5ebb88e8b44d07f8b4f340c08692e1423d3719faf48d24d
+size 2949
