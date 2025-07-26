a448d6dac095b17aca9ccd39726498b7f0317e9d
윤규성 <rbtjd0312@naver.com>
2025-07-26 16:40:29 +0900

    feat : 변호사 가입 승인, 이메일 중복 검사
    Former-commit-id: d23f7f19f890026c8a9837cdf8279e0cb78353b3

---
+++ backend/src/main/java/com/B204/lawvatar_backend/user/admin/controller/AdminController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:92745bc0a3a5523f7828207c9eb588e898ef515d8c60fd3483d90dff45a2682b
+size 1453
--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91da9a366dba732886aeccdbf9fcb44edee296eb03d253bc37bf960ae7281744
-size 1710
+oid sha256:d814a6b5c5028fce615da32b50919c0cc569aaf5a7f558c1f27dc8d1b115ffad
+size 2212
