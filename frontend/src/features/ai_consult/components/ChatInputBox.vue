e555684728fc5b067923649ab865c8ee45ebfaaf
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-07-28 15:12:31 +0900

    fix: ai상담 화살표아이콘 변경
    Former-commit-id: 13cf356ba89b0514c03ae2a9cedc3481618e2f6d

--- frontend/src/features/ai_consult/components/ChatInputBox.vue
+++ frontend/src/features/ai_consult/components/ChatInputBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4355166922df14f16a6b0d35101e33f3e4a1c0f6a65499e21cbd891fb188731
-size 1983
+oid sha256:0439aacee6f295350dcc445eca4a8ce15aa33c567e572384aff20206598478c3
+size 2162
