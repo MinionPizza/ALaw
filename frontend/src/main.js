749157d356a79381002c060ff98d066b22774739
김태인 <inintae63@gmail.com>
2025-07-28 10:04:08 +0900

    feat. msw 활성, 비활성화 추가
    .env.development 파일 추가로 안에 값 disabled, enabled로 변경시 msw 활성/비활성 설정 가능
    
    
    Former-commit-id: cb24ffa4032981be28cc94a0183286794ac2915e

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,3 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-import '@/assets/Common.scss'
-
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser')
-  await worker.start()
-}
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc40cb5ca8d7d163aac1c3f9b7bcaeeea5a0f10aafa35287ba5b852dc30677b9
+size 804
