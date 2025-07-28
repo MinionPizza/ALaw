d129c97e44154a7594ce59f40a334536f3de375c
jeongyeon <wjddusdl921@naver.com>
2025-07-28 14:53:12 +0900

    fix: auth 관련 pinia 수정
    Former-commit-id: c6c3d371bebd9702a013dcde9baf67c9802bbcbd

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b68ee7ae726f829e2fafc15256405d8ef7a3096d107669c0edf449b6ccf359d
-size 1208
+oid sha256:eb3ac69cc912430c2968d1ab1f4e36772c6f1ee2f2aad411b1b4499b7a08cd3c
+size 1494
