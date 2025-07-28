97283a8d5ec721888d5494926a527f2304fabdb4
전해지 <wjsgowl0224@naver.com>
2025-07-28 17:39:52 +0900

    refactor: 기존 openVidu 코드에서 인증로직 제거
    Former-commit-id: d38bc21bf352eb9f5432debaca24f04fbbaab656

---
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/dto/ParticipateRoomResponse.java
@@ -0,0 +1,17 @@
+package com.B204.lawvatar_backend.openvidu.room.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParticipateRoomResponse {
+
+    // Field
+    private Long id;
+    private String token;
+
+}
