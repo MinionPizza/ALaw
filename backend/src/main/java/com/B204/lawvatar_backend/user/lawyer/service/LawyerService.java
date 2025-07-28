2257adcc6c1109a6f52765a02fb81ed04d05dad2
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-28 17:54:58 +0900

    feature : Lawyer 테이블, 등록 수정 Dto에 image 추가
    Former-commit-id: cf193f200b911e26378e4e8534c6a48f14c9537f

--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2108147d5f17acb1df0f6585b188437d3c07634cb96b955731830ee3b19b7e6
-size 3612
+oid sha256:0c8127763d3dc0e69ee0acfb631696b724ecb3304edb0304bd22a5c176879c69
+size 3781
