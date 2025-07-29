32ab4ee810fd2b1653c4f89e8c25f76aca7d63fa
전해지 <wjsgowl0224@naver.com>
2025-07-29 14:05:17 +0900

    feat: schema appointment 테이블 start_time, end_time nullable로 변경
    Former-commit-id: cdef1f00d93c73faa0fd5bb068474c0096af9fd9

--- lawaid schema.sql
+++ lawaid schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e0b80a75ac1db455e8ef8bd2eca5be2988dd37ed89d5dc20de16cd6f0bc0b6c
-size 5452
+oid sha256:24d0de4a91061d8b0214fde551dd45e11454e55282babb10046dc0c945a549f6
+size 5461
