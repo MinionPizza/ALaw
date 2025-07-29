d97f089fc3c0026625fd18396825e7486d84ebe2
전해지 <wjsgowl0224@naver.com>
2025-07-29 13:52:00 +0900

    feat: lawyer 테이블 photo 컬럼 추가
    Former-commit-id: ba92cce569bf4e10f22c1a414c7ae49d91543391

--- lawaid schema.sql
+++ lawaid schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea5b156e445077fde9f8ed8844d49cecc6425ce580ea3bbd41ae95f2755287c5
-size 5436
+oid sha256:5e0b80a75ac1db455e8ef8bd2eca5be2988dd37ed89d5dc20de16cd6f0bc0b6c
+size 5452
