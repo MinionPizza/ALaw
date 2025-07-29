8b930d1dbe136e4da444002f8453a71a02f92b38
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-29 15:32:49 +0900

    fix : 로그인, 회원가입 front 통일 중
    Former-commit-id: ee5a025ea71c051c4d3d2d89dd7dff8eba15d9f0

--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/entity/Lawyer.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/entity/Lawyer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7792e12eab2a6c48aa2b5244808196ac66ff831582aee0746a9b31ad9fc2b1b
-size 1343
+oid sha256:e6c9c046b5c56c3d8011ef618cb81b4f07f7fe96f8bdafa365a198d939f576c2
+size 1349
