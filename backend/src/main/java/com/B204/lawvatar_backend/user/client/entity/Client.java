7378efd0c7a379ad683af1a98aad14a4a224f2e0
전해지 <wjsgowl0224@naver.com>
2025-07-29 15:25:56 +0900

    feat: 엔티티, 스키마 수정사항 반영
    Former-commit-id: 3b83d5825b0170f67799ef73e3d29b2a6fe0b6bd

--- backend/src/main/java/com/B204/lawvatar_backend/user/client/entity/Client.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/client/entity/Client.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0ca55c68ccb6e2e18e3a84f23a3127af12f0222c9608bd9fe5ab31b1668bbf4
-size 1034
+oid sha256:60db1469c3f088ce7f727de109af2dcadc1fa7a6605d2b0de20204ff7f66559b
+size 1092
