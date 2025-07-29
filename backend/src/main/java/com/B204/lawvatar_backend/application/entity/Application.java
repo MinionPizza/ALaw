177932842aefa83891f1c6d7c74240f101c6edba
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-29 17:40:25 +0900

    fix : front 확인(로그인, 회원가입) 완료
    Former-commit-id: 56a4782c029401453b523d5cbf8d13a589f4974f

--- backend/src/main/java/com/B204/lawvatar_backend/application/entity/Application.java
+++ backend/src/main/java/com/B204/lawvatar_backend/application/entity/Application.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c7f7029beb0350a375f12456fa7542de232001fe9c504aa68fcb4662501545e
-size 1233
+oid sha256:e9c5d57e5eb446489c5b5ffa5a2b0c82419c4c4c0e682127c809dfe866ff7a14
+size 1221
