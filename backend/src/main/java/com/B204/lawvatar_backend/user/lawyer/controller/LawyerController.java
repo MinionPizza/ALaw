ce42bed7f129efdce6f9a28111aaabd43c94722f
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-30 17:50:37 +0900

    fix : lawyer login error 분기처리
    Former-commit-id: 0fe2a5176478e31c42ea6d9709bd4e3abb1d6ee5

--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/controller/LawyerController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/controller/LawyerController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f124a56f72c4948ff06a16a48024b911dcf68f2731bd0e367d7ad7463b4b9dba
-size 9253
+oid sha256:c76684707c37cc4f6eba5192aa2fa9adde19fa073d6a1635001d9ce45bc6d72b
+size 9782
