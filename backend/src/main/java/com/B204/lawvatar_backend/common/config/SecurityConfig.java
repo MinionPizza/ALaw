bf7bb1900ecf8320f74828617c14be008adf4a2f
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-30 14:54:32 +0900

    feat : 조건부 변호사 검새, SWAGGER 의존성 추가, 경로 허용
    Former-commit-id: f2c9106222660a6ce4d7e8eeed7020cd4e11540a

--- backend/src/main/java/com/B204/lawvatar_backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/lawvatar_backend/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9945f37e4c4cac607af6702d5af16f525597dded0c04efe78eb8e7c86672bc5
-size 11874
+oid sha256:a496d36bd758fa078db87c61dccea1cbad0da6fc2053c5acbbeacf7b32028384
+size 12137
