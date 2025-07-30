b67d021801a251d8eec061e1f5aec437d274dab9
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-30 13:14:37 +0900

    feat : lawyer, client 계정 삭제
    Former-commit-id: 7cc9c292bea745c1d9d223712d36030417fd4019

--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/controller/LawyerController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/controller/LawyerController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa1e80eb37120f55b36e150a53195bba9e6ef1088c79801965f6261925856d57
-size 8472
+oid sha256:41d7a0a38b638890b7b80f4383117cd1c97d7651d213e6ebbd9ea0edcfa11142
+size 8761
--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c8127763d3dc0e69ee0acfb631696b724ecb3304edb0304bd22a5c176879c69
-size 3781
+oid sha256:6ebe0ce13e93feece55d012dfd407ed23c77d97cd562fbe167c49cc8d38c3cbf
+size 4458
