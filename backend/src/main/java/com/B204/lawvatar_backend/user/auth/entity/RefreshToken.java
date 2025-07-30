577b05c3f3172c09db0ce095551aac5baf8394b3
김태인 <inintae63@gmail.com>
2025-07-30 12:31:13 +0900

    fix. merge 오류 해결
    merge 후 저장안한 실수 해결
    
    
    Former-commit-id: d248561be26d971c416eaecfc9a5a44901d8021f

--- backend/src/main/java/com/B204/lawvatar_backend/user/auth/entity/RefreshToken.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/auth/entity/RefreshToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b392c9c315b3dbf4cbb2efd940c2a12ae558a18d12ec1e0511ed936570314c7
-size 2221
+oid sha256:9a1b2db28195b57831308181aeff4866c8cb8f1fd175971f8fc54e970c10b89b
+size 2011
