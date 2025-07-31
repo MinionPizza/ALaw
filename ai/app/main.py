312c8ce7abebcaa7dc0b37ce3acefcfcbcb90b7e
grayson(방승철) <bbt1250912@gmail.com>
2025-07-31 15:51:22 +0900

    chore: CORSmiddleware 추가 및 analysis service test 코드 수정
    Former-commit-id: 135e507f4bfd737495c75b9de4aa5f2ae4d243eb

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9e735df98246b6b3a87c4eb4f72495584e8e40a472bad7e8ed44c6e71040e2a
-size 1587
+oid sha256:eca58ed38cba0b788de12595edce3ed1418f12e3bf2d8defdbe2227c21fb3196
+size 2003
