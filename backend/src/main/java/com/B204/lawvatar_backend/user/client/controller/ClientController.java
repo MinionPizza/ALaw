130f2d293da81a565251a1b5824905481f5c80ab
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-07-31 17:36:14 +0900

    feat : 상대방 정보 조회 1-4-1. 의뢰인 -> 변호사 상세 조회 1-4-2. 변호사 -> 의뢰인 조건부 조회 (상담 내역 있는 경우만)
    Former-commit-id: 772bda856cb28c671d5ebe00061d60b77a751233

--- backend/src/main/java/com/B204/lawvatar_backend/user/client/controller/ClientController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/client/controller/ClientController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2344179ba28b0e9803444c5f646d59bd0a2a718f37e1baa4229bd9686a1c54fa
-size 2701
+oid sha256:182f93992b8bbcbeee05bceb90a30ca17e31aa025e5f3236779dabd5a4da7f90
+size 4286
