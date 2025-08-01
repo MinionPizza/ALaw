f214f85a5d7cce1e1790187307fe7736b3f70c9a
윤규성 <rbtjd0312@naver.com>
2025-08-02 01:35:15 +0900

    fix : 1. PreAuthorized 적용 (Client, Lawyer, Apppintment) 2. lawyer 회원가입, 로그인 구조 변경
    Former-commit-id: 173ffe865a619d69740f55fcae10d5cc679d3ab5

--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/controller/LawyerController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/controller/LawyerController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa0a096b7cc09f592569dacb68e27ad323f5d3d0d11cf395b18a0d96354cb17d
-size 10333
+oid sha256:9026636b9ff766870a1922b45c27582ddd1bb59b257d31cdaa1f33710a4f61ef
+size 7690
