820aceb390467e601058948ad2e8dd250ae9ab96
윤규성 <rbtjd0312@naver.com>
2025-08-01 23:46:53 +0900

    merge request - error clear
    Former-commit-id: 153e74e179fd114f7f323732f91e313150652e61

--- backend/src/main/java/com/B204/lawvatar_backend/appointment/controller/AppointmentController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/appointment/controller/AppointmentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68c47b632fe630b7c895c953a99c9258b7bfba2fb895705374c47ab98253d614
-size 9360
+oid sha256:c7422606b0cc1d29c70e0d86469ff8d51f471aea0fc42e6543ca6e8e447bc1b4
+size 9611
--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:439aec6bf12c2183c4aa749d1ba9acd3f849a7ff686a0f962a7daf53c1bdb9ae
-size 5735
+oid sha256:39cb05cd815b7ddb726f5db2de97f0e01e39c5dafeee445463673c005e631314
+size 5730
