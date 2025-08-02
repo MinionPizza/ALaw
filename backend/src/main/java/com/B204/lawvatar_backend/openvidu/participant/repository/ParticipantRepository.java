6d5a6b82b1711096c182435b37ead6439bd41f6d
전해지 <wjsgowl0224@naver.com>
2025-08-02 20:33:05 +0900

    feat: 테스트 통과
    Former-commit-id: 8a2c52337e4834b4e9423da9a498bd368f4dd669

--- backend/src/main/java/com/B204/lawvatar_backend/openvidu/participant/repository/ParticipantRepository.java
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/participant/repository/ParticipantRepository.java
@@ -4,4 +4,6 @@ import com.B204.lawvatar_backend.openvidu.participant.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+    void deleteByClientId(Long userId);
+    void deleteByLawyerId(Long lawyerId);
 }
