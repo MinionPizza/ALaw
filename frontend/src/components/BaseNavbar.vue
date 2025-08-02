b905b0a2775a7c3d8ebb79ccef12877911a80f33
jeongyeon <wjddusdl921@naver.com>
2025-08-02 23:34:07 +0900

    feat. social login 경로 수정
    Former-commit-id: dff29cb1732b99235c9a30657b2f287aeb1f06c3

--- frontend/src/components/BaseNavbar.vue
+++ frontend/src/components/BaseNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7663009bc31b9537614d27b68db0c5bdf12b44953466b6744af3a360f822a493
-size 6404
+oid sha256:d890deba4674ed9d480b8acc1a7ffc0711d3327831904b353b3bfc248ca70a3a
+size 6403
--- frontend/src/features/reservation/LawyerSearch.vue
+++ frontend/src/features/reservation/LawyerSearch.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bd95813ce2f71b5c23617a03c8400cb831a5287beaca479b88bf1ffb62c41dc
-size 2345
+oid sha256:d02c4735ba6e38ea34e871db5f7fd65906a7ad890b3c8b6aa58fc6d75474f487
+size 2450
