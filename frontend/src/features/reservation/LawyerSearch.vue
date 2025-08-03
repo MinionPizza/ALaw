db78dc5eb983141a752976e7e82f7217bb734745
jeongyeon <wjddusdl921@naver.com>
2025-08-03 10:53:25 +0900

    feat. lawyer filtering 검색 기능 추가
    Former-commit-id: b892b727eedcbcee1c65fc8c5efb4bac2e9b4145

--- frontend/src/features/reservation/LawyerSearch.vue
+++ frontend/src/features/reservation/LawyerSearch.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d02c4735ba6e38ea34e871db5f7fd65906a7ad890b3c8b6aa58fc6d75474f487
-size 2450
+oid sha256:4702444a71cc0f35f07324ffe1e744a427927d1b9dadb338786251f393b78425
+size 3660
