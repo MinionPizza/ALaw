cd6f72641d20b629771c162b2c4611882f68c5be
jeongyeon <wjddusdl921@naver.com>
2025-08-04 17:43:24 +0900

    sytle: reservation 파트 디자인
    Former-commit-id: 08a438446e56951eac544d63da114e5c0e17df36

--- frontend/src/features/reservation/DetailReservation.vue
+++ frontend/src/features/reservation/DetailReservation.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93a99e18420e4f0ccc929ba537bd6c7103cddb081866de48e6539de2e1ab1a41
-size 7286
+oid sha256:3767505efab72bee369e853de2f49bfbcf8b8e9dfc509a7e0297edd6129a8f1a
+size 8244
--- frontend/src/features/reservation/LawyerSearch.vue
+++ frontend/src/features/reservation/LawyerSearch.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ebd07593705ae2d8b7099b001fbbb5b32186493d3973d10565506d3b4191a2a
-size 9987
+oid sha256:8f3c68635e2002bbccb6f2fc931892318488c0c51533abf0cf1b58c7e11d9b2e
+size 10181
