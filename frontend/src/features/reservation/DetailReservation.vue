1e02dc3bb30082e1998599d2018b8d7405673a86
jeongyeon <wjddusdl921@naver.com>
2025-08-04 15:42:21 +0900

    feat. 디자인 구현
    Former-commit-id: 456887f0d665a6d0cb64ebaa8b27531fafc8d179

--- frontend/src/features/reservation/DetailReservation.vue
+++ frontend/src/features/reservation/DetailReservation.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b00e3cec51b505a1f60fbfe3cbd7db53429d357be9829385667ecb42ff7d7eea
-size 4642
+oid sha256:93a99e18420e4f0ccc929ba537bd6c7103cddb081866de48e6539de2e1ab1a41
+size 7286
