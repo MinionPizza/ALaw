04e733c10c483fb149edcf326960879683a6d2ad
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-04 14:43:18 +0900

    feat:화상상담 진행중
    Former-commit-id: ff72a195f196d4b2ccdecb8c45dc0e2f76d86779

--- frontend/src/features/videoconference/user/PreviewUserView.vue
+++ frontend/src/features/videoconference/user/PreviewUserView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97e5bbb6ec99c9b3050420bcb06d827ec67ebc0040ea83732007fd6e10a7313c
-size 6044
+oid sha256:c509a6f07af8d6a2a60eb1bfdb465c55aa59fdcfcca3a6dc8f6944d1cccaaa23
+size 10502
