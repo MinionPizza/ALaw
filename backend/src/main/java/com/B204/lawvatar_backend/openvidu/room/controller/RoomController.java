9dfd477c5890e44b91f9a3a8edf9838e2d1820ad
전해지 <wjsgowl0224@naver.com>
2025-08-04 13:57:24 +0900

    refactor: Room 도메인 리팩토링
    Former-commit-id: 7b126e69a1beafd0e6233d2de336302ebdcd4590

--- backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/controller/RoomController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2efee75488d9d8ff981f50965c1d30ba51f7fd89bc76e3edd9b2fb94c1cc842
-size 4831
+oid sha256:51b380303de357959c414fe3e38b1a1c5b4c66eef1079b817ca076a2f7de335d
+size 8283
--- backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/service/RoomService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1217151c2a4da6a7e2ea5ef34718066d2c118ee068ac9ee229eb62c97e7fd489
-size 12747
+oid sha256:22a5cfa86891ab16d2e785ea3d62a83d72ac0b9a48c2673a0724fa0f3f162fa6
+size 13018
