fff6739857f0118b753708362a83d1d42cea5a24
전해지 <wjsgowl0224@naver.com>
2025-08-04 12:39:43 +0900

    fix: 변호사 담당 상담 신청서 전체목록 조회 uri 명세불일치 수정
    Former-commit-id: 2c8f9c8f9a88747e6080564a8dab87905d3a2182

--- backend/src/main/java/com/B204/lawvatar_backend/appointment/controller/AppointmentController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/appointment/controller/AppointmentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:552fed6e34f88aa170d0c8568c2ebe1de9efe095f7b8cce895fcaa3f802ad800
-size 8765
+oid sha256:1e1072406d30684c694fbf005e5e6751b8f6abd69ce0be04a0e205a2530480fb
+size 8768
