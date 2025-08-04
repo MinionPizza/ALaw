df49f1f9a304648167a4719748ea0451e25a1ca4
전해지 <wjsgowl0224@naver.com>
2025-08-04 19:58:28 +0900

    feat: 상담신청서 수정 REST API 개발
    Former-commit-id: 3ab64dbe9ea15040182542f26e80e809479e0810

--- backend/src/main/java/com/B204/lawvatar_backend/application/service/ApplicationService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/application/service/ApplicationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d56df1d45592962f6d8db5cd8e56d87f6af687855f072b0e00a0fa160329648
-size 7879
+oid sha256:4fceec487626a155443963b402d9ea9a77548a8d6051a4a22f6281fe1f511ada
+size 9978
