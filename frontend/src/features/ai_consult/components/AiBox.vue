995ef8c75335f2e7e47f24c2213c271246f9696d
김태인 <inintae63@gmail.com>
2025-08-04 15:35:40 +0900

    feat. 사건 개요 요약, ai 사전 상담 구현
    api 연동 및 통신 성공후 데이터 처리
    
    
    Former-commit-id: 450016612ed7b8b1ef186fe68b238545d9db02de

--- frontend/src/features/ai_consult/components/AiBox.vue
+++ frontend/src/features/ai_consult/components/AiBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa37d35217ebf1fd4c569035dc45fee833653486d48a874de4070bbf9ff26409
-size 3882
+oid sha256:72ee5fcd0f374fa720325ae322b798f3116b186df4d0b48a9412360bd4b95bf3
+size 4192
