b2171e124476911c41a8389453fa0d85124f53da
Owner <dlsxo14700@gmail.com>
2025-08-04 20:37:24 +0900

    feat. 관리자 기능 추가
    관리자 로그인 제외 기능 추가
    
    
    Former-commit-id: 817ce46e10abf59789946d61c87ae5785639f5c4

---
+++ frontend/src/features/admin/LawyerCertifications.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b869260b89c69d82e076ca75735088cec629be39338461a42978b217ae977424
+size 3228
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b4b3848e4b0c922ca927891abf6944b24dc8cdcb533b91076714b13f264eff6
-size 4146
+oid sha256:d912ffde8248465c6e379c937d6b842bf6f780d872cb6ea020ce3deed7c3cb5f
+size 5582
