c2d637c08be34e40dfc4c260c9d90c0a2bf0a706
김태인 <inintae63@gmail.com>
2025-08-04 16:09:26 +0900

    fix. callback uri 엔드포인트 수정
    Former-commit-id: c8bc6713ce88b808db39ba8b8a8b708e0f309950

--- backend/src/main/java/com/B204/lawvatar_backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/lawvatar_backend/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b77dc27cef5133f179e974ee3e6e774803ba69d25fb7c37d5dc9790bb5de9ef6
-size 12178
+oid sha256:beed388be3cdb0e3873d3c7a0576b2d0f3be65a87e724c8af5981460548f7cfb
+size 12188
