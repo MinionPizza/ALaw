1aecee338f99ea1361b802af005f6633d60cef04
전해지 <wjsgowl0224@naver.com>
2025-08-04 12:34:40 +0900

    refactor: 에러 응답하는 형식 및 위치 리팩토링
    Former-commit-id: fc51732c3ce2b02ebceba7daa162b32ca60bbbeb

--- backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/controller/RoomController.java
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37cb72caa9526a83ef0f8c0fc5757c4f86975e7d4a3c0676efde5d5a1b3a698f
-size 4068
+oid sha256:c2efee75488d9d8ff981f50965c1d30ba51f7fd89bc76e3edd9b2fb94c1cc842
+size 4831
--- backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/service/RoomService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/openvidu/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f44457ddcfc64dd705bef7c74b0dbed491d8ade2358be667b403c77deeae45f
-size 13762
+oid sha256:2644761bf4b06ce96bd7280b3e2c286a8b97f0d57f2965567fd1eb82a0842e2f
+size 13447
