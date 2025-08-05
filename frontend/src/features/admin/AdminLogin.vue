cb613b0b43a4c6038583632d4f6ac25c7cf09599
김태인 <inintae63@gmail.com>
2025-08-05 13:28:58 +0900

    fix. 로그인 부분 오류 수정
    redirect, api 오류 수정
    
    
    Former-commit-id: 2801062dcfd92c3c62a890f6a768010b89951ebc

---
+++ frontend/src/features/admin/AdminLogin.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8b5b89a4a67fd8954bbacf2147a2d0544a679112873c30b1a8e201a4e7e0026
+size 4356
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06c57c6bd50e0828390e723a893ce1fcf379f8d45d493fa8ec1a77aa2d50b26f
+oid sha256:ccaf104e19d187e60b8e0de485eb8be4118fd747129a05edce7d10862371c346
 size 5584
