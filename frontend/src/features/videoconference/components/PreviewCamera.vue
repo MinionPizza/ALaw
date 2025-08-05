c5ae713ab837cf6dcdd80ff3473331a4081b6a55
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-05 13:33:57 +0900

    feat: 화상상담 미리보기 변호사 디자인 구현
    Former-commit-id: fd7adb48516877e53ac7f4335e24754d61a6db17

--- frontend/src/features/videoconference/components/PreviewCamera.vue
+++ frontend/src/features/videoconference/components/PreviewCamera.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c8511b536ce6eb80ee041d5494ec2b352ef6af4d5dab2eb778641a2ce895647
-size 2367
+oid sha256:6cfba445dcf817d8e6b4571b808ec68af7d718249477bf0f6fb80769edf61d0d
+size 2570
