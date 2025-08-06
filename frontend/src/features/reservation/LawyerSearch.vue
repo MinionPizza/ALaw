658a48f90183acf0c109ac361175573e129e1548
김태인 <inintae63@gmail.com>
2025-08-06 16:34:35 +0900

    fix. 예약 로그인 문제 해결
    usertype 변수명 변경
    
    
    Former-commit-id: 30d1f5cb52946cbae1c1d4b01de19b4186aa8bae

--- frontend/src/features/reservation/LawyerSearch.vue
+++ frontend/src/features/reservation/LawyerSearch.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92a57a0a2d965f22aabea7d12c30d098a8382e3762c8a2828011ac2c7863ef28
-size 9867
+oid sha256:1f0a3c322ae90b400e60c480da8933c101e9fb8817b25926274bc8262ca618a1
+size 9865
