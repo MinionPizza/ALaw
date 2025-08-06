f58f031a46c7420d18cc2df28eaaa95e911116f3
김태인 <inintae63@gmail.com>
2025-08-06 17:00:33 +0900

    fix. 페이지 이동 오류 해결
    Former-commit-id: e77b7cca34bfd868ad1bc8fcbe4a7831b87d556c

--- frontend/src/features/videoconference/components/PreviewCamera.vue
+++ frontend/src/features/videoconference/components/PreviewCamera.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cfba445dcf817d8e6b4571b808ec68af7d718249477bf0f6fb80769edf61d0d
-size 2570
+oid sha256:42f571c1c0651da54a72bf2e115b4bccebf68b634624b57d92a6a4a16f6b3898
+size 2784
