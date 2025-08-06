22737e7ed7a52a7726a9dae000d2135f7c90d1c4
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-06 13:51:13 +0900

    feat : API 4-1-1 appointments/me 에 상대방 이름 추가
    Former-commit-id: 3bb6acf26bf8c6ab17b96f86d206993be29ca18b

--- backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
+++ backend/src/main/java/com/B204/lawvatar_backend/user/lawyer/service/LawyerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b12ca02e6b8b3a8d3a1dd22d52c84a4c3d038ac43f324e1a9292e18647ee1cb3
-size 9655
+oid sha256:26ba109128c3062b975666237de233d43ba45f284fc3bbd70b1ad3d3a583d7f2
+size 9838
