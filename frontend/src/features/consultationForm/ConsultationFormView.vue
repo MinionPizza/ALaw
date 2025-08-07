3106a7f7e1bddb98189232ca4e679f970cb0da1a
김태인 <inintae63@gmail.com>
2025-08-07 14:15:38 +0900

    feat. ai 상담신청서 기능 추가
    복사해서 수정하기 기능, AI로 수정하기 기능 추가
    
    
    Former-commit-id: a87abcd9eb7dd1b8212026c9168bf0623798f581

--- frontend/src/features/consultationForm/ConsultationFormView.vue
+++ frontend/src/features/consultationForm/ConsultationFormView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaf1ddd728991ad1192ba25e21b5b946dde3d2b5b780e9f242704738fde6522d
-size 8102
+oid sha256:47d43ffda8f95b4076362aff48b7d0e3d4f1dba75702b1c7a83383d65947446a
+size 8085
