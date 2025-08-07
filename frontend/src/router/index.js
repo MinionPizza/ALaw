a8d3e554b86af81aae40de7af3363069e70e3b65
jeongyeon <wjddusdl921@naver.com>
2025-08-07 16:18:00 +0900

    style. 변호사 조회 부분 레이아웃추가
    Former-commit-id: fef6b8f4ffa246bf9501d8a1730e90981d96a348

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed58ec300e7f2520a5c89143b058760f22aee1d8ca853226329722b4203aaec1
-size 8213
+oid sha256:8fe0ef4cb434c995f0911347ee1a1ac3af56ec5f9a213c803796171deea72918
+size 8268
