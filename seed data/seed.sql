fb691b622e96450a9918c6decadb6a8b6c5d30c9
전해지 <wjsgowl0224@naver.com>
2025-08-07 13:02:43 +0900

    feat: 변호사 seed data insert문에 비밀번호 누락돼있던거 추가
    Former-commit-id: 7551ae0ce09b1911a9a7cb4331ec83fa354ce765

--- seed data/seed.sql
+++ seed data/seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba06de61e67f64d2c0de28dafe09e4bad440498c40178636cc2142bb3019ef4
-size 19842
+oid sha256:e568c138e0a6cfc0eec73cae2304edb02080d1fe07799504c41251071fa6a7c2
+size 20202
