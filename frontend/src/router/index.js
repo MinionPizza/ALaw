e80d10ea2581606910a10ed913c703822e24d747
김태인 <inintae63@gmail.com>
2025-08-07 14:30:40 +0900

    fix. 작은 오류들 수정
    비밀번호 찾기 임시 추가
    
    
    Former-commit-id: a3a5ccb92fe70bd316f79fcc7b132c945fd58ffe

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42a6ffdd547ae8424c078b0a6224e5629f960ce3a7f23830c83ac7b663761e8b
-size 8004
+oid sha256:ed58ec300e7f2520a5c89143b058760f22aee1d8ca853226329722b4203aaec1
+size 8213
