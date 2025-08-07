73953168432c02092b27d48504c37da86276305e
jeongyeon <wjddusdl921@naver.com>
2025-08-07 14:49:27 +0900

    fix. 이미지 상대경로 지정
    Former-commit-id: 4811a86864d890f044fedb12843312f4a0fb0596

--- frontend/src/features/profile/user/UserMyPage.vue
+++ frontend/src/features/profile/user/UserMyPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00f645e2fdd7f2893c3543b7ed9da25a299c650fa579fb10ec01a8013294edd1
-size 8780
+oid sha256:040ba4a6fd2dc936b1c088b47dce3cdfebe904faae58be6007e37de629e436aa
+size 8735
