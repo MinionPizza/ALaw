72d29b9f610d7ce1f93df208084ba6c1b830cbe6
전해지 <wjsgowl0224@naver.com>
2025-08-08 01:18:03 +0900

    refactor: 콘솔출력 코드 전부 삭제 및 주석 개선
    Former-commit-id: 70572360de58959f355c833142f7b7dd497755a8

--- backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
+++ backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef8b4a5d6b7744150d9ecac73c63be04eb00d64c99c922b5be0c81ccc5ac4df9
-size 15156
+oid sha256:6b7cd10f81add0640cabfe79ce63c82dffe95783b8d662fc33c4b23724101b54
+size 14361
