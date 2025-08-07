6234c9499cb112bbfebe7164922f71e6211c95a4
jeongyeon <wjddusdl921@naver.com>
2025-08-07 14:49:27 +0900

    fix. 이미지 상대경로 지정
    Former-commit-id: 32695a81645756bbd681f522c2c765404719cc30

--- frontend/src/features/reservation/LawyerSearch.vue
+++ frontend/src/features/reservation/LawyerSearch.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17a8bc20af6711d2e6d1ad87fc38a39d027c58625d013522f80982e117324d94
-size 16100
+oid sha256:ea75942980b29bb587bedc3ed8535eab856a7912ac10f1e2966ac6a9e3a5a73f
+size 14132
