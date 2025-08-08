017bc7968f6eb7b040ac5c87e7850676efbc3487
전해지 <wjsgowl0224@naver.com>
2025-08-08 14:51:31 +0900

    fix: 화상상담방이 열려있지 않은 상담에 대해 화상상담 참가요청을 했을 경우 발생하는 NullPointerException을 핸들링하는 코드 추가
    Former-commit-id: df7e4a5199b6527afb969cf9ac1c097e03ec8aee

--- backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
+++ backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef8b4a5d6b7744150d9ecac73c63be04eb00d64c99c922b5be0c81ccc5ac4df9
-size 15156
+oid sha256:21f8b3aa30ec3dac1cd43ce4c3346c86c74da262e13d3a2aaadfe1044ad0bfbb
+size 15404
