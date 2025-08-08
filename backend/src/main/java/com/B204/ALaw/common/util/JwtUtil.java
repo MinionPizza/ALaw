5b3063faf85f135d1a758eb93025b3a297c9a7a9
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-08 15:12:43 +0900

    fix : accessToken 유효시간 15분 -> 1시간
    Former-commit-id: f50eadccd5ec801563af85135b886001e1e41fed

--- backend/src/main/java/com/B204/ALaw/common/util/JwtUtil.java
+++ backend/src/main/java/com/B204/ALaw/common/util/JwtUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3663eaa01bae8601535b6c42d25d1bdfd87c76bd0841d8b21d173cf99817de
-size 1612
+oid sha256:bfb39f0845102e69280fa1423470314351a5b2d1b3a61b7a59dd10a433821d26
+size 1614
