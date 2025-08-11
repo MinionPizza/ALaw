374aa8e16f5522e122f4564dd85c67c725e3bcf0
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-11 15:36:25 +0900

    fix : JsonUsernamePassFilter 변호사 상태에 따른 로그인 분기
    Former-commit-id: 0eee38610da7ea41726c1b8acbfb6689f8da4696

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:308fc3be0ed95319517cde7df3fb862ee240be7031f70c46c3d30b0e0cc3bbee
-size 15718
+oid sha256:60883c3a5f3bcd06befd2e8516b3e1cca4e792e04e1473f71fe8d25c0d4b5445
+size 15826
