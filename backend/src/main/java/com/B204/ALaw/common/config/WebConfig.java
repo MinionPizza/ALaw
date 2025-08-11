5bc5e036518db064ceed9d4eca2036c84e6baec3
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-11 11:18:28 +0900

    fix : 1. OpenAPI 추가 2. allowedOrigin 추가
    Former-commit-id: dedbe6ecb7fe1da2176d43a70b8b404c854c0ae9

--- backend/src/main/java/com/B204/ALaw/common/config/WebConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:247cac3ca1f5d169f33d6fc603a9abdd0dd1de5e5f64749754e5606480dbc30c
-size 948
+oid sha256:d7f524a236c99ba967ce9ebe3ca91e83eb206f2a3a56eba5521687b30298dad7
+size 1221
