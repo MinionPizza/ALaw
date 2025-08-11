e9322c7c38d2a7a4f6fb55f7e320a39ac188c210
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-11 10:55:21 +0900

    fix : allowedOrigin 추가
    Former-commit-id: f75932ceb0fc75efa81cde20ba2bf3a86799722c

--- backend/src/main/java/com/B204/ALaw/common/config/WebConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:996ee79c046fbcd05a62f52513c1aa5b7f4711bbfa975d8d0dbbf9cd2e013d6c
-size 923
+oid sha256:247cac3ca1f5d169f33d6fc603a9abdd0dd1de5e5f64749754e5606480dbc30c
+size 948
