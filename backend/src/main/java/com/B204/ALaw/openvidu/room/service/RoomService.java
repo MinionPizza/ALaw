2457de74cda14954979bec387725a21306bb3f6d
전해지 <wjsgowl0224@naver.com>
2025-08-11 13:22:29 +0900

    fix: 화상상담방 나가기 API 에러 고치는 중...
    Former-commit-id: 379c2d528daba13e86706e466cc1b42d060316b3

--- backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
+++ backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d70b6764c18be0a7fe3ee663176dc06528e4f677c0cb88cbdc5fbd7f92c7660d
-size 14695
+oid sha256:152106711d37ad8b69a05adffe025e0876ff4c5b2d14e132488456382761f0f9
+size 14775
