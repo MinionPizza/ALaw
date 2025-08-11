f9342c687c133880baa83f0b76e7b4e0c31f48a8
김태인 <inintae63@gmail.com>
2025-08-11 17:28:54 +0900

    fix. 이미지 오류 해결
    Former-commit-id: 2d77356bbb7d98c80f6ec66da82c7ad9415095df

--- frontend/src/features/profile/lawyer/LawyerProfileUpdate.vue
+++ frontend/src/features/profile/lawyer/LawyerProfileUpdate.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b0c4fd3299b9366e06c8d68fc4fd380d21943b3ad8a88ebbec53efb7cb4fca8
-size 5588
+oid sha256:ea28fe6b4901c8e5233a454450520546a58e782fe38a75a2650a04d81ecca5b3
+size 6546
