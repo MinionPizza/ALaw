165892ba5f29b1e8c759c020573d51735d39f7b2
전해지 <wjsgowl0224@naver.com>
2025-08-11 12:38:05 +0900

    feat: openVidu 토큰 권한 관리자로 변경
    Former-commit-id: 17e17a6c56c584de8dd5772f2db38a0f2d2f2b20

--- backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
+++ backend/src/main/java/com/B204/ALaw/openvidu/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:204e5f50eb7a02e8d8d48d51c638004632c098978292d67c748d3f3edce8d9b1
-size 14609
+oid sha256:d70b6764c18be0a7fe3ee663176dc06528e4f677c0cb88cbdc5fbd7f92c7660d
+size 14695
