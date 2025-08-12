13fad249aa9447509c805e13d8705d87beb16307
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-12 16:42:58 +0900

    feat: 그림리셋추가
    Former-commit-id: 55ef5556a11329ffd12762cdf1edc8688363a01b

--- frontend/src/features/chatting/components/ChatComponent.vue
+++ frontend/src/features/chatting/components/ChatComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a8fe67ed43d9586002f02467de8d21f874476a53af84fb6d9a277d1ff5c5145
-size 4389
+oid sha256:4c6a8ffb1db3c1a012c57f513f7afe81ae71c8eb050c9c04754f3afdde7ec327
+size 4390
