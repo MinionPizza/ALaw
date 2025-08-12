003acad283d19f1cb796ab2fd79320bfe3e7e2e0
intae0099 <dlsxo14700@gmail.com>
2025-08-12 23:27:40 +0900

    fix. 배포용 주소 axios 오타 변경
    Former-commit-id: 31e0b2f5183021184e1d3e82b06af74a953e8766

--- frontend/src/stores/cases.js
+++ frontend/src/stores/cases.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e095cdfa2fe3223d1425be2998c7798d69a568fb54a5ec303b6474b70902d78
+oid sha256:286d98d89d8cb57ed8e641b18172fef77d64a6f1a988fdf56bb66d61693c927c
 size 4138
