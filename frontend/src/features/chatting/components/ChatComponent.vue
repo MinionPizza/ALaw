d6bd6cc619e87515e4ea22aac4aa5757ed79876e
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-12 13:29:36 +0900

    feat:화상상담 그리기기능
    Former-commit-id: 2cde3400b707d423212a8c90be06119c9a61af26

--- frontend/src/features/chatting/components/ChatComponent.vue
+++ frontend/src/features/chatting/components/ChatComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb56acadc4ec13da1a76b6d9d70b43d3cfce0b2814b4b66e29aa9ede5e7897f0
-size 4393
+oid sha256:8a8fe67ed43d9586002f02467de8d21f874476a53af84fb6d9a277d1ff5c5145
+size 4389
--- frontend/src/features/videoconference/MeetingRoom.vue
+++ frontend/src/features/videoconference/MeetingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e0e8d729a6f3ff5defd337a0792b8250e7213004e54173e4481f4afb372ed0e
-size 18926
+oid sha256:678387130cefdf39320b191f63f696f30f5989bae976d3c93aa9e75e87cf4f52
+size 21188
