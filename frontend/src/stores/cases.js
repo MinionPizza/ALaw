ff35a478f7a3771e3596f8bfe38627bcda9a9d03
intae0099 <dlsxo14700@gmail.com>
2025-08-12 23:22:14 +0900

    fix. 배포용 axios로 변경
    Former-commit-id: 5b9a8df256b3c83de0328297746045378b8e4dea

--- frontend/src/stores/cases.js
+++ frontend/src/stores/cases.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:714e2bc09ecd8783cadf584ecf470da52091ec2e6611f889393ccef75bf80418
-size 4224
+oid sha256:4e095cdfa2fe3223d1425be2998c7798d69a568fb54a5ec303b6474b70902d78
+size 4138
