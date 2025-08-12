60083f1a4b0ed62107753fbb84b593d8f7b00731
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-12 10:46:42 +0900

    fix:화면공유 수정
    Former-commit-id: 2125403167eb9f1b02b37df8d7809258ab057d30

--- frontend/src/features/videoconference/MeetingRoom.vue
+++ frontend/src/features/videoconference/MeetingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7fa517f0a127054eefa7a88afc9bdec2ec658415847dd8d2d4a7d31565e84f
-size 18920
+oid sha256:7c8c709f87745deec88c3eb660061d786256d8ffd72ad713ecc5242592088e99
+size 18917
