572a4b468e6963f853ff7c2c6467b1dfdce27ae1
윤규성 <rbtjd0312@naver.com>
2025-08-12 23:53:54 +0900

    fix : redirectUrl 이원화
    Former-commit-id: 889cec8659c516deeb705a3793dd90636a8190a5

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e3f6632be2487b00738d8f4bcc40696adef8b4f6fc330a2ca75208951eb1110
-size 15624
+oid sha256:11282ece18e12380f7c2474817fe753a2c9802fe073b9bf994bdc9452116bade
+size 15866
