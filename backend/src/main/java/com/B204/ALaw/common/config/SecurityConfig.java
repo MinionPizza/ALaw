8c548048dfcaf0fc169b3051d894a7c0cb0fe8de
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-12 15:55:03 +0900

    fix : localhost - domain redirectURI 구분
    Former-commit-id: fe7cb3750f77c052b8c4311afbf9f1c6b6eddff7

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1fe63dda32260b850745e392656306ad7858353298598f1b34a1eec82ef1f8d
-size 13855
+oid sha256:c570bbe39837f81a789c05d0e776b4abc175ca16f48e8e2545dc83d67f8c9e44
+size 15357
