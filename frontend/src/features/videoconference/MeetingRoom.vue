0e6a71e74dfaf73b236fa062027dd5e8a29f54c3
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-12 14:32:35 +0900

    fix:화상상담 오류 수정중
    Former-commit-id: 0dcaa0021d980cf4c5a4fd6661058ab621a739fb

--- frontend/src/features/videoconference/MeetingRoom.vue
+++ frontend/src/features/videoconference/MeetingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:678387130cefdf39320b191f63f696f30f5989bae976d3c93aa9e75e87cf4f52
-size 21188
+oid sha256:1e5e59fa94d022e45944f2fcac463c11bb3cd32755e5c64c277e742317ad3887
+size 24696
