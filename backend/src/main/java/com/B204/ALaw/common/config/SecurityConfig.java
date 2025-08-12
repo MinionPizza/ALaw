b78a15b517fb36a888a9a4104aa37238285ac4b7
윤규성 <rbtjd0312@naver.com>
2025-08-12 19:57:49 +0900

    fix : SecurityConfig 원복
    Former-commit-id: ecbb38d80db3f12ec27ead6848f2769f4f1146ee

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2570575820a2f561133ec2e4b16e463aaadc564b0e37ea25616fa0362fc339ec
-size 15508
+oid sha256:b0da94153626d5c5ca7d73b7befcfeabf227b4a3c4660ec50b019467d07500ef
+size 14052
