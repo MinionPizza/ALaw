f91b9b3bb04f3d50b725e67e41247ad662958c4b
김태인 <inintae63@gmail.com>
2025-08-12 13:31:29 +0900

    fix. 카메라 점유 해제 구현
    화상상담 대기방에서 다른 라우터로 이동시 카메라 점유 해제
    
    
    Former-commit-id: 6ad664a9d86726c58cc44400abca7562b72ffe99

--- frontend/src/features/videoconference/user/PreviewUserView.vue
+++ frontend/src/features/videoconference/user/PreviewUserView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cac6fb72ac7decb2930eb1e89dbc433f660bfc36a13d275c39ce88c9f9d14e9
-size 15051
+oid sha256:9cc70962f2e77200a9673ce130be24e456c8ebf2ea138a664966073fc1cf779a
+size 15907
