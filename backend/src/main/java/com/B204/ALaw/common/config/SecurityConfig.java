8e2de13265ac0d0c7276105c715ad7820725aa35
윤규성 <rbtjd0312@naver.com>
2025-08-12 21:56:48 +0900

    fix : frontBase 동적 주입
    Former-commit-id: 43d94f161da3e365a0164629dfce9ef97544f9ce

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0da94153626d5c5ca7d73b7befcfeabf227b4a3c4660ec50b019467d07500ef
-size 14052
+oid sha256:3e3f6632be2487b00738d8f4bcc40696adef8b4f6fc330a2ca75208951eb1110
+size 15624
