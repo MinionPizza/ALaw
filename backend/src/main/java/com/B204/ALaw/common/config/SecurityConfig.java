cf51fcbf34a21f90c7276728f89b56dd891a5f5f
김태인 <inintae63@gmail.com>
2025-08-12 17:24:36 +0900

    Revert "fix : 동적 로그인 수정"
    This reverts commit 3f2c2493e3ea7569680155b4e7310e61f9e4e431 [formerly 893168525cacb31c3db016276fbf4efbf038670a].
    
    
    Former-commit-id: dc94755d024ad8f7041530ff9de8e9479a93a0a4

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2570575820a2f561133ec2e4b16e463aaadc564b0e37ea25616fa0362fc339ec
-size 15508
+oid sha256:c570bbe39837f81a789c05d0e776b4abc175ca16f48e8e2545dc83d67f8c9e44
+size 15357
--- backend/src/main/java/com/B204/ALaw/common/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/B204/ALaw/common/filter/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73d7f5817599da70fd1be79490caaa2cbd6bcca59e09695c505ed7e0b46e103a
-size 6220
+oid sha256:a2638726988606b85105440713d62778759d6e517f71337d4cc4bdaf84119e76
+size 5839
