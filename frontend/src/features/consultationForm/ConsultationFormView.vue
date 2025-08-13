3b30583acf797cab257be16f49f2b75f7cfc6406
jeongyeon <wjddusdl921@naver.com>
2025-08-13 13:20:50 +0900

    style. 예약 모달 디자인
    Former-commit-id: 10b69831564623045edfb574f3cc4b3305cac597

--- frontend/src/features/consultationForm/ConsultationFormView.vue
+++ frontend/src/features/consultationForm/ConsultationFormView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0f3bd8884277d60b363a2e7b647fde2bbd1f76c130efde86b4d4b94337b5641
-size 8175
+oid sha256:a1f78132fd8c4bf353e0fb818353b90b153bc98609a40fce5ac3bdcaa321d0f2
+size 8172
--- frontend/src/features/consultationForm/component/ConsultationForm.vue
+++ frontend/src/features/consultationForm/component/ConsultationForm.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:711b1897bfcffc8ea4798ff8b60c0785e6fec5044ddc853bda6e6e5f49fe66cd
-size 6117
+oid sha256:20e2e77e4064583361cfbb2996d3979dbddac087d62bd223a5a50777f4066dc6
+size 5708
