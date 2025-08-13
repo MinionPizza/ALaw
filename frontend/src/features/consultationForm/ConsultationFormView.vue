7d80be4561eb58c38b38eac4d30086f8436b6c30
intae0099 <dlsxo14700@gmail.com>
2025-08-13 23:35:09 +0900

    refactor 상담폼 수정
    Former-commit-id: 33c43259811f44568f5876fe9f35d59278327843

--- frontend/src/features/consultationForm/ConsultationFormView.vue
+++ frontend/src/features/consultationForm/ConsultationFormView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1f78132fd8c4bf353e0fb818353b90b153bc98609a40fce5ac3bdcaa321d0f2
-size 8172
+oid sha256:63877ba6f16e240b47b9dd6769b8d2c8f551c3ea722d87809266c59ef1575338
+size 8573
