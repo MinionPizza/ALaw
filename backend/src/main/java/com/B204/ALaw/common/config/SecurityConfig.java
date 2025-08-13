6e98c6dd86dd5a66c9ed195495a4fec536059d2f
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-13 17:17:40 +0900

    fix : refreshToken samesite Strict -> None
    Former-commit-id: 594a9ecee5efbb7245545eddb278c1bd930fcda7

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11282ece18e12380f7c2474817fe753a2c9802fe073b9bf994bdc9452116bade
-size 15866
+oid sha256:96fa848f96c0550579a2cdefd8ebe5627657947b2e7ca8a37f6580cc5ac78434
+size 15862
