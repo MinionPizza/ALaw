3574859a559258282b5a248d0feafb6febbc417b
김태인 <inintae63@gmail.com>
2025-08-13 13:35:16 +0900

    fix. 사전상담 법령태그 추가중
    Former-commit-id: 1d4cd9004432e698dd436624eeeb7370517a49f2

--- frontend/src/features/ai_consult/components/AiBox.vue
+++ frontend/src/features/ai_consult/components/AiBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93e313a97b3cd70ecc3d6f3f5f8c4ac0853401d25bef42655c3808e6f715104b
-size 9058
+oid sha256:94bb20a89e17e869d075e3f3d748d34a274cd0c9d96bd77b1f127542b13989e7
+size 9569
