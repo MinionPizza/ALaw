e789c28060c5881fd453ceb7bd4f83f9a4fef56f
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-13 17:53:21 +0900

    fix: ai상담 디자인 수정
    Former-commit-id: 780dd76c4444cdd0aabcec5c976153310ef4f48f

--- frontend/src/features/ai_consult/components/AiBox.vue
+++ frontend/src/features/ai_consult/components/AiBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11278bab2dfb63ce53d62ab2d0cf4f2bdd9fddc4dc1386fab700cf78399fff1f
-size 10007
+oid sha256:cda7243590a81e4695e7508294626f3c63d5d27c41008d03f76067603e2dbda8
+size 10663
