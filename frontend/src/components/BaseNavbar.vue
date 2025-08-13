b78cec2bc5b440219066262619db862d6c222fd7
DESKTOP-HA97GMI\SSAFY <dbsrbtjd0312@gmail.com>
2025-08-13 17:54:36 +0900

    feat : refresh TOken
    Former-commit-id: 227de9963fb746836406de4d73524770f265ab87

--- frontend/src/components/BaseNavbar.vue
+++ frontend/src/components/BaseNavbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4d767ccc7fc96c35df843edecb3eff7570b4029b6bf07251d29a27a8d92b91c
-size 8416
+oid sha256:19876820424b866cbc2b8ab2f4b2bddfe921aeec79982142572f8da91dc71aef
+size 9597
