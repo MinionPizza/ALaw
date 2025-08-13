dd6bea8158e5473feee2b0330c17041cbaf24b91
윤규성 <rbtjd0312@naver.com>
2025-08-14 00:25:57 +0900

    fix : refresh enable
    Former-commit-id: 15d1260c4fdd69907713a127a3c03bfbc9d425f1

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e831772b82dc714a13ec9bc8df9762596b31822fa665097ac0abbd91a1c7b2b7
-size 16113
+oid sha256:e575cf3c5e5e87580ce6e3117cf9161544c7b8cc982ecf54e150adff360a8a1b
+size 16133
--- backend/src/main/java/com/B204/ALaw/user/lawyer/service/LawyerService.java
+++ backend/src/main/java/com/B204/ALaw/user/lawyer/service/LawyerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:008578121c3420dd618d0f735d8f067cf550013d618058754ee8fcd57cf3690c
-size 9523
+oid sha256:6d447579aae450e87e7d86221b0d1c598129ac0ba1df96dc7f6e78f00d5c9139
+size 9533
