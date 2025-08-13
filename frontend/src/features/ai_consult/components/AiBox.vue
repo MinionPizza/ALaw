e6dd092f958942a0cff46d4396c87afa62b13776
김태인 <inintae63@gmail.com>
2025-08-13 16:48:15 +0900

    fix. admin 레이아웃, ai사전상담 법령링크 적용
    Former-commit-id: 9c5ec5af5b4da5790c7f325ac051a87e45bbb372

--- frontend/src/features/ai_consult/components/AiBox.vue
+++ frontend/src/features/ai_consult/components/AiBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94bb20a89e17e869d075e3f3d748d34a274cd0c9d96bd77b1f127542b13989e7
-size 9569
+oid sha256:11278bab2dfb63ce53d62ab2d0cf4f2bdd9fddc4dc1386fab700cf78399fff1f
+size 10007
