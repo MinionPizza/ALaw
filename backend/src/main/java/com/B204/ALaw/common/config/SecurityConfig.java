3e9054b7dc59d77ed1f3fb692d6737428d6000c8
윤규성 <rbtjd0312@naver.com>
2025-08-14 21:32:16 +0900

    fix : redirect 도메인으로 설정
    Former-commit-id: a63c0f9da4a06414204c38c005164b853cf9906b

--- backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
+++ backend/src/main/java/com/B204/ALaw/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e575cf3c5e5e87580ce6e3117cf9161544c7b8cc982ecf54e150adff360a8a1b
-size 16133
+oid sha256:3ecb1436e5ba9cfb9879e51bcdcdd1921ec047111f46274c0e67277ea8e64729
+size 16329
