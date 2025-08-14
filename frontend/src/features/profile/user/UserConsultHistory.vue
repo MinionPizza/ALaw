fc672ff67853a47ef293f2b406edbcbafba5c529
jeongyeon <wjddusdl921@naver.com>
2025-08-14 10:16:19 +0900

    style. 마이페이지 디자인 완료
    Former-commit-id: 1306d9c21334abd935dd5d8a8e4bbf581a59b4e1

--- frontend/src/features/profile/user/UserConsultHistory.vue
+++ frontend/src/features/profile/user/UserConsultHistory.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0bedce9b290ea6ffd4f835dc91c343967536d580a4a269106cb1aa3d802a248
-size 10932
+oid sha256:d4f223f9970a0b981dcc4eb9585aadc9d6b11d2b7bd6aadbe3a6cda1f9796863
+size 10956
