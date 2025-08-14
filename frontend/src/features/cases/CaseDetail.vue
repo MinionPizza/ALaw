d8871c1ce489af2700feacdd07f7f0dc6ae13474
김태인 <inintae63@gmail.com>
2025-08-14 14:18:36 +0900

    refactor. casedetail 디자인, 사전상당 result form 변경
    Former-commit-id: 1e0c380e42f6659053979cf73b7ca10e5d7bdc93

--- frontend/src/features/cases/CaseDetail.vue
+++ frontend/src/features/cases/CaseDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2c5c757579caa47a1b5cc78c6bee056b9a97435ea9996a335a5283e47f944e6
-size 6683
+oid sha256:32a4ee5ca7514cf01e3f6216ba91854d3539dbc0837382353814fbafbc0f18f5
+size 6708
