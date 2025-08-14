a15d1a9a3080b179526deeb13325bc8bbe890757
김태인 <inintae63@gmail.com>
2025-08-14 10:25:28 +0900

    refactor. 상담신청서 디자인 수정
    Former-commit-id: 8e16ca8679ba76604ee8c14a57c5746938f3892b

--- frontend/src/features/consultationForm/ConsultationFormView.vue
+++ frontend/src/features/consultationForm/ConsultationFormView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1f78132fd8c4bf353e0fb818353b90b153bc98609a40fce5ac3bdcaa321d0f2
-size 8172
+oid sha256:73641ade5c85446eb447aebebfbedb2c90ebbb637b45541704deac1be82cc366
+size 8604
