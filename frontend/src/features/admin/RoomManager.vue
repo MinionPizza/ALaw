a6086fc62ed5981e4f9ef91bdf72e14cd58a83f9
jeongyeon <wjddusdl921@naver.com>
2025-08-14 15:13:03 +0900

    style. 관리자 페이지 디자인
    Former-commit-id: 923e2227be86a531fb73a0f84e7b53ada907e45a

--- frontend/src/features/admin/RoomManager.vue
+++ frontend/src/features/admin/RoomManager.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d415ab049468fda830f00230ca6c8a5a1f46fea0e0cde3779b0a446c879a07c2
-size 3070
+oid sha256:76b154860070c706b0e738b0aaf02a30edba2a0df70106fc0695ac1f07d6e3e8
+size 3301
