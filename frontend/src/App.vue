f89f7c58c5e0a84f238511b427dd3e27653fb49d
jeongyeon <wjddusdl921@naver.com>
2025-08-14 15:13:03 +0900

    style. 관리자 페이지 디자인
    Former-commit-id: 54a0e08d5694d0df91f1bda5151f7dbc6ee54efc

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f3a98db1e358a7acd679a4da1cdac736ff0fccfc58549ba4931bc0e0f5759a8
-size 778
+oid sha256:87a9a84c3543a88bd34ac1d69cf2f13e863f4e62a84fefb70a297ac5c85411cc
+size 873
