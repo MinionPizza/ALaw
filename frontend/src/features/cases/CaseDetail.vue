2dd083aea7b79c09ec5978be0a8662d5a3601882
김태인 <inintae63@gmail.com>
2025-08-14 14:46:03 +0900

    refactor. 판례상세조회 디자인 수정
    Former-commit-id: 4dd460a7a55a12b6157c61e00f13d38a924c861d

--- frontend/src/features/cases/CaseDetail.vue
+++ frontend/src/features/cases/CaseDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32a4ee5ca7514cf01e3f6216ba91854d3539dbc0837382353814fbafbc0f18f5
-size 6708
+oid sha256:6dc8d17273ffd9fb13c3dc1fc2462c78590a0e1bdf8072cb81f67bc6e90107f1
+size 6725
