f4c2c2316b8a2cf537e93bb0551129177a719612
김태인 <inintae63@gmail.com>
2025-08-17 16:58:13 +0900

    fix. patch iscompleted 추가
    Former-commit-id: 082f1a55060be14c03b2716bd251d3bf5d65c02b

--- backend/src/main/java/com/B204/ALaw/application/controller/ApplicationController.java
+++ backend/src/main/java/com/B204/ALaw/application/controller/ApplicationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1cc223a451da06bac49517d7603deb6bcc06f2f01a0a18a5713301c07989642
-size 15823
+oid sha256:c397513fd3e6ab6d62ad025f4bab9d663c9121ec84512c081d9bbc46da7d6b10
+size 16004
--- backend/src/main/java/com/B204/ALaw/application/service/ApplicationService.java
+++ backend/src/main/java/com/B204/ALaw/application/service/ApplicationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d9d7080e05f7286af6e96e17f391979d2fc494fbf3715837fd5d2ebc84af8fe
-size 9948
+oid sha256:b477e1c3ee295122ba11225b0d69980b5e3d0bd4bf3bdbf8ceba1d8d31942799
+size 9996
