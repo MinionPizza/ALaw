51232c78ae3b26da58f832c98e67ae7d1d186873
김태인 <inintae63@gmail.com>
2025-08-17 17:02:05 +0900

    fix. patch iscompleted 수정
    Former-commit-id: 5a126d6604297e42a1097d833b4b29ffc6d1bbd1

--- backend/src/main/java/com/B204/ALaw/application/service/ApplicationService.java
+++ backend/src/main/java/com/B204/ALaw/application/service/ApplicationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b477e1c3ee295122ba11225b0d69980b5e3d0bd4bf3bdbf8ceba1d8d31942799
-size 9996
+oid sha256:46d14d6b67992ade1928575c0b97c494634bff5ff635b2ebf5a57c115c2ba927
+size 10017
