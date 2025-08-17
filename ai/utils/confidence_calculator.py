24a8c8ce0c1517ab62fbd766cb2bb0cafa54a11f
grayson(방승철) <bbt1250912@gmail.com>
2025-08-17 21:38:46 +0900

    fix: 신뢰도 0.3 고정되는 문제 수정
    Former-commit-id: 4541b46ad9e5d23667c4fadb8d947014242817d3

--- ai/utils/confidence_calculator.py
+++ ai/utils/confidence_calculator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a340160c329507ef620554e21ee94aa6a37ccfe84e3661d4cbc29f68794dc91e
-size 7536
+oid sha256:67dd4ccc025fec789e4cb49038d109d8fe031c0d8253fcd58e3c7a02b5b79321
+size 9141
