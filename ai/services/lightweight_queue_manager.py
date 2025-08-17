a3b5c4b0d38539f2883820872de8bbaa2953305e
grayson(방승철) <bbt1250912@gmail.com>
2025-08-17 21:32:55 +0900

    fix: 리소스 95% 확장
    Former-commit-id: cd21f3f74c2ff8067c1286a2b90476a17d037c5e

--- ai/services/lightweight_queue_manager.py
+++ ai/services/lightweight_queue_manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19f05840acbe1285e2e8860f7435b43c37acf57ab5265ba431f83f451e3b84b8
-size 23711
+oid sha256:cdb95688bd3e240865d5cccc184d65b44b31a49372f34a55dcf18091d277d5b6
+size 23718
