e7a2c9c2dddd86ed7063f382c731a3b1130ed213
intae0099 <dlsxo14700@gmail.com>
2025-08-18 00:43:21 +0900

    fix. 실시간 채팅 세션 전달 오류 해결
    Former-commit-id: 4382e05188139fff224263af36328d62517ff96a

--- frontend/src/features/videoconference/MeetingRoom.vue
+++ frontend/src/features/videoconference/MeetingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16eb8da171119b3cd5524d175082fe089e8886a6d09d46a6d17595a06b3cd162
-size 28599
+oid sha256:fdaa92c9dce6e5e662378b580cab1b86562fc7416600b323bc427a68aef7bdc3
+size 28555
