7da285c0ef008d857fb2179e14f8a2bb0885cc42
김태인 <inintae63@gmail.com>
2025-08-17 15:00:38 +0900

    feat. 형사, 횡령죄 추가
    Former-commit-id: ed89b1d22de7289660b721c9ce8ff9e138649e4f

--- frontend/src/features/reservation/LawyerSearch.vue
+++ frontend/src/features/reservation/LawyerSearch.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2220f561baa008f7d026eca2037aeb90e3f653d54062e3e18cbfd3164a41689
-size 16666
+oid sha256:e79aab3172965d677b802a7204e6a1b8b637e9583b8ce9d2bc538fd5e86921ca
+size 16678
