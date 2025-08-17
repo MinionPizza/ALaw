eb407815e9264cb0899efb82ff250be1d8f4fb66
김태인 <inintae63@gmail.com>
2025-08-17 14:46:38 +0900

    fix. 법령 링크 수정
    Former-commit-id: 8e413cee601bfaf0810a027e67f308d3d5c7b7fd

--- frontend/src/features/ai_consult/components/AiBox.vue
+++ frontend/src/features/ai_consult/components/AiBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cbeb79daf4f7ef982d11989461686638c6b1fe5f1818194257335dc1b7e8ae5
+oid sha256:bd1e125b61a6d826e5109512e3c8b7cdce0eafd7995e41594d302f5d5d66e4c9
 size 16902
