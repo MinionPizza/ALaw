c25e33a41217b9e6c39bc3539becd9714d7aa438
sseni <ctyom08@gnu.ac.kr>
2025-08-18 23:58:35 +0900

    fix: 메인페이지 디자인 수정
    Former-commit-id: c05f04f58fc9be71f86b139d4dc96abdd67a43ae

--- frontend/src/features/main/MainPageView.vue
+++ frontend/src/features/main/MainPageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:907b80adff5d50982cb64d6943d4610bf16bc5583e3d20cddda79e9c3b1be132
-size 11840
+oid sha256:b72bb60b52fe13e7ee9ea348561941e870343f0556edad9bd7cdc48f5af6421d
+size 12889
