5174d5fc78ca5ecfee523e7d24735e7353dc17da
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-19 14:44:17 +0900

    fix:ai상담 로딩게임추가 + 상담신청서에서 마이페이지로 이동
    Former-commit-id: 9c0eda48fe3f6b6cb479eb89b7524d441f3c5417

---
+++ frontend/src/features/ai_consult/components/CircleTimingGame.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20a0191958b815ef9f1b31c0b4c6eea79b13b360edfb47b7e374c2cf3886e2f8
+size 6563
--- frontend/src/features/consultationForm/component/SaveAlertModal.vue
+++ frontend/src/features/consultationForm/component/SaveAlertModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a11f4967c6f6ed1e7aa31063ed0083141ee8ad09b28246ba48f3a346466bccbb
-size 1391
+oid sha256:eb3ba20a6ef6f1c57c9643ad5a9bf9d325c3943f105dfe061e7d756bc143cd65
+size 1345
