4ccc51ead5cc6da3384b987f32999d4bb03f613b
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-19 22:52:28 +0900

    alert디자인
    Former-commit-id: 60db979e541336680d149821b390e68eedbfbadf

---
+++ frontend/src/components/BaseAlert.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a36c8ecef4c80cc254cac7ebb63b7758f1ff1e95f33afc681ed15171da0028e
+size 1611
--- frontend/src/features/ai_consult/components/AiBox.vue
+++ frontend/src/features/ai_consult/components/AiBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22755a5fff78069495a1e5ed59c817fe292f9a75b864d206537ce78698f2fae6
-size 17453
+oid sha256:8cc67402848827dc4c48fb7254eebb1d3ea9864b3bf5e22f36b13ffd2574fff0
+size 17144
