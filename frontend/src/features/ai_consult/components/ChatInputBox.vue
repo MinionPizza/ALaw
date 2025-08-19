8b867d6fc09fc979385aa6cdcafe61ac7ec4a6d5
DESKTOP-DLOPF19\SSAFY <rnjswkdms123@naver.com>
2025-08-19 14:44:17 +0900

    fix:ai상담 로딩게임추가 + 상담신청서에서 마이페이지로 이동
    Former-commit-id: 57269c86d3a8c2cda6ba6021448fa951f2168a95

--- frontend/src/features/ai_consult/components/ChatInputBox.vue
+++ frontend/src/features/ai_consult/components/ChatInputBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34a5ff5f75a4a37c7c03fa482697adde6d2e42b2a49e3fe2ea6ba269715c8e6b
-size 3005
+oid sha256:9b22a6ba0aecc444d7ac88839975f1ebce65d085ada7046e953b06dcb54f5bf6
+size 3571
