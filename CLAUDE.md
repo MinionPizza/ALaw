fed0153567c610796a354cfd5193ebc4fb025c49
grayson(방승철) <bbt1250912@gmail.com>
2025-08-26 14:26:15 +0900

    fix: Kiwi 초기화 오류 수정
    Former-commit-id: 11da157f06532f5f70a82b885a7f7a7f71c02bd0

---
+++ CLAUDE.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e65207b16739d5a0bec02a8165285346b49aba40ddf56334cb4e00f1f51c55f
+size 16403
--- ai/services/bm25_service.py
+++ ai/services/bm25_service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b638f417bc28c91aab20ab6bb9604d69544605c9cb2aec25148645dbd688fc
-size 4992
+oid sha256:fcb4f20b03f63737654b22e669d8a3215550b52479e2cbcf6d05df8b35de5bf4
+size 7312
