bee321e9ac09cf2ce2f1cf3ff9e7dafaea3cc5b9
grayson(방승철) <bbt1250912@gmail.com>
2025-08-26 14:55:03 +0900

    fix:  bm25인덱스 파일 추가
    Former-commit-id: 071213a541220ad94b54750e32d57fda1cb71bd6

--- ai/services/bm25_service.py
+++ ai/services/bm25_service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcb4f20b03f63737654b22e669d8a3215550b52479e2cbcf6d05df8b35de5bf4
-size 7312
+oid sha256:f14933d39c2e9d370ff701cefcccae51af10c5fb076ce7de9992d6f5f63a5df3
+size 7483
